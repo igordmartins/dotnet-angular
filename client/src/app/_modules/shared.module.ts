6bec8bce92da4b96cde8357143e24381276ac057
Igor Martins <igordavillamartins@outlook.com>
2022-10-24 19:50:54 -0300

    added loading spinner, cache

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
 import { TabsModule} from 'ngx-bootstrap/tabs'
+import { NgxGalleryModule } from '@kolkov/ngx-gallery'
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 
@@ -14,12 +16,16 @@ import { TabsModule} from 'ngx-bootstrap/tabs'
     ToastrModule.forRoot({
       positionClass: "toast-bottom-right"
     }),
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    NgxGalleryModule,
+    NgxSpinnerModule
   ],
   exports: [
     BsDropdownModule,
     ToastrModule,
-    TabsModule
+    TabsModule,
+    NgxGalleryModule,
+    NgxSpinnerModule
   ]
 })
 export class SharedModule { }
